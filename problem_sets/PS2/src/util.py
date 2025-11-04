2980783d053027d108d8df5ba7c754e0041fcd81
MarioRG29 <rodmariogom@gmail.com>
2025-11-04 19:10:07 +0100

    My solutions to PS2

--- problem_sets/PS2/src/util.py
+++ problem_sets/PS2/src/util.py
@@ -90,7 +90,7 @@ def plot(x, y, theta, save_path, correction=1.0):
     # Plot dataset
     plt.figure()
     plt.plot(x[y == 1, -2], x[y == 1, -1], "bx", linewidth=2)
-    plt.plot(x[y == 0, -2], x[y == 0, -1], "go", linewidth=2)
+    plt.plot(x[y == -1, -2], x[y == -1, -1], "go", linewidth=2)
 
     # Plot decision boundary (found by solving for theta^T x = 0)
     x1 = np.arange(min(x[:, -2]), max(x[:, -2]), 0.01)
@@ -100,7 +100,7 @@ def plot(x, y, theta, save_path, correction=1.0):
     # Add labels and save to disk
     plt.xlabel("x1")
     plt.ylabel("x2")
-    plt.savefig(save_path)
+    # plt.savefig(save_path)
 
 
 def plot_contour(predict_fn):
